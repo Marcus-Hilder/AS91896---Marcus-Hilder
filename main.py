42b245448a80b5b68fa5989a14f51ba389b16a9a
marcusschool <mh22085@mh22085@my.westlake.school.nz>
2025-06-16 15:09:23 +1200

    dictonarys

---
+++ main.py
@@ -0,0 +1,45 @@
+import easygui
+
+task = {
+    "T1" : {
+        "title" : "Design Homepage",
+        "description" : "Create a mockup of the homepage",
+        "Assignee" : "JSM",
+        "Priority" : 3,
+        "Status"   : "In Progress"
+    },
+    "T2" : {
+        "title" : "Implement Login page",
+        "description" : "Create the Login page for the website",
+        "Assignee" : "JSM",
+        "Priority" : 3,
+        "Status"   : "Blocked"
+    },
+    "T3" : {
+        "title" : "Fix navigation menu",
+        "description" : "Fix the navigation menu to be more user-friendly",
+        "Assignee" : "",
+        "Priority" : 1,
+        "Status"   : "Not Started"
+    },
+    "T4" : {
+        "title" : "Add payment processing",
+        "description" : "Implement payment processing for the website",
+        "Assignee" : "JLO",
+        "Priority" : 2,
+        "Status"   : "In Progress"
+    },
+    "T5" : {
+        "title" : "Create an About Us page",
+        "description" : "Create a page with information about the company",
+        "Assignee" : "JLO",
+        "Priority" : 1,
+        "Status"   : "Blocked"
+    }
+}
+
+team = {
+    "JSM" : {
+        "name" = "john smith"
+    }
+}
