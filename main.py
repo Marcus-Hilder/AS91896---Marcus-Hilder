9ca10d6b43f467e7523e88747f37ea06acb9e17c
marcusschool <mh22085@mh22085@my.westlake.school.nz>
2025-06-17 14:20:16 +1200

    start of pick

--- main.py
+++ main.py
@@ -40,6 +40,79 @@ task = {
 
 team = {
     "JSM" : {
-        "name" = "john smith"
+        "name"  : "john smith",
+        "Email" : "John@techvision.com",
+        "task"  : ["T1","T2"]
+    },
+    "JLO" : {
+        "name"  : "Jane Love",
+        "Email" : "Jane@techvision.com",
+        "task"  : ["T4"]
+    },
+    "JSM" : {
+        "name"  : "Bob Dillon",
+        "Email" : "Bob@techvision.com",
+        "task"  : ["T5"]
     }
+
 }
+def menu():
+    """
+    displays main menu and return user choice
+    """
+    options = {
+        #"add_task"    : add_task,
+        #"remove task" : reomve_task,
+        "edit task"   : edit_task,
+        #"search_user" : user_search,
+        #"print all user's"   : show_all,
+        "exit"        : exit
+    }
+    get_input = "Y"
+
+    while get_input == "Y":
+        msg = "what would you like to do"
+        title = "games Database Choices"
+        choices = []
+
+        for items in options:
+            choices.append(items)
+        
+        selection = easygui.choicebox(msg , title , choices)
+    
+        get_input = options[selection]()
+    
+def pick(type):
+    if type == "task":
+        Choice_list = []
+        for key , task_info in task.items():
+            Choice_list.append(task_info["title"])
+        choice = easygui.choicebox("pick a task" ,"task picker" , choices=Choice_list)
+        for key, task_info in task.items():
+            if task_info['title'] == choice:
+                return key
+    elif type == "lables":
+        Choice_list = []
+        for key  in task.items():
+            Choice_list.append(task_info)
+        print(Choice_list)
+    elif type == "user":
+        print("how tf did u get here")
+    
+    else:
+        return menu()
+    
+
+
+
+def edit_task():
+    """
+    """
+    TYPE = "task"
+    print("susceful entered task edit.")
+    user_request_type = pick(TYPE)
+    user_request_
+    for key ,task_info in task.items():
+        if user_request == key
+
+menu()
